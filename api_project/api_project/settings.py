c122a8f0e7ee2279efe6aa2491d20cabdc7b8dd4
Brunokayitar <brunokayitare85@gmail.com>
2026-03-02 02:32:52 +0200

    Add token authentication and permissions

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',      
+    'rest_framework',  
+    'rest_framework.authtoken',    
     'api',  
 ]
 
@@ -117,3 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # for browsable API
+    ]
+}
