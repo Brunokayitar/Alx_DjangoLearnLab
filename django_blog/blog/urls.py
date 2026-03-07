17bd8772f772d380c18f6a91933804f792f58a44
Brunokayitar <brunokayitare85@gmail.com>
2026-03-07 02:05:41 +0200

    Update tag URL pattern to use slug: tags/<slug:tag_slug>/

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 
     # Tagging and Search
-    path('tags/<str:tag_name>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
+    path(.tags/<slug:tag_slug>/., views.PostByTagListView.as_view(), name='posts-by-tag'),
     path('search/', views.search, name='search'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -138,13 +138,13 @@ class PostByTagListView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        tag_name = self.kwargs.get('tag_name')
-        tag = get_object_or_404(Tag, name=tag_name)
+        tag_slug = self.kwargs.get(.tag_slug.)
+        tag = get_object_or_404(Tag, name=tag_slug)
         return tag.posts.all().order_by('-published_date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tag'] = self.kwargs.get('tag_name')
+        context['tag'] = self.kwargs.get('tag_slug')
         return context
 
 def search(request):
